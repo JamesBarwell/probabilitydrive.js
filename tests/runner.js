c7447a117fdfaa5af68d04ef6dacd7658eb84993
James Barwell <jb@jamesbarwell.co.uk>
2014-02-13 22:02:14 +0000

    Add tests for basic use, add package.js and Makefile

---
+++ tests/runner.js
@@ -0,0 +1,128 @@
+var assert = require('assert'),
+    mocha  = require('mocha'),
+    pd     = require('../probabilitydrive').probabilitydrive;
+
+describe('probabilitydrive.js', function() {
+
+    var pdInstance;
+
+    beforeEach(function() {
+        pdInstance = new pd();
+    });
+
+    describe('observe()', function() {
+
+        beforeEach(function() {
+            // Start each test from the root page
+            doJourney([
+                '/'
+            ]);
+        });
+
+        it('should add a newly observed URL', function() {
+            doJourney([
+                '/test',
+            ]);
+            var data = pdInstance.getData();
+
+            assert('/' in data);
+
+            var rootData = data['/'];
+
+            assert.equal(rootData.length, 1);
+            assert.equal(rootData[0].url, '/test');
+            assert.equal(rootData[0].count, 1);
+        });
+        it('should increment the count of already observed URLs', function() {
+            doJourney([
+                '/test',
+                '/',
+                '/test'
+            ]);
+            var data = pdInstance.getData();
+
+            assert('/' in data);
+
+            var rootData = data['/'];
+
+            assert.equal(rootData.length, 1);
+            assert.equal(rootData[0].url, '/test');
+            assert.equal(rootData[0].count, 2);
+        });
+        it('should ignore repeat calls with the same data', function() {
+            doJourney([
+                '/',
+                '/'
+            ]);
+
+            var data = pdInstance.getData();
+            assert(!('/' in data));
+        });
+        it('should chain with other module functions', function() {
+            assert.equal(typeof pdInstance.observe('/').determine, 'function');
+            assert.equal(typeof pdInstance.observe('/test').determine, 'function');
+        });
+    });
+
+    describe('determine()', function() {
+
+        beforeEach(function() {
+            // Start each test from the root page
+            doJourney([
+                '/'
+            ]);
+        });
+
+        it('should predict a simple repeat path', function() {
+            doJourney([
+                '/page1',
+                '/'
+            ]);
+            assert.deepEqual(pdInstance.determine(), '/page1');
+        });
+        it('should predict multiple paths when count weightings are equal', function() {
+            doJourney([
+                '/page1',
+                '/',
+                '/page2',
+                '/page3',
+                '/'
+            ]);
+            assert.deepEqual(pdInstance.determine(), ['/page1', '/page2']);
+        });
+        it('should prioritise paths that have a higher count weighting', function() {
+            doJourney([
+                '/page1',
+                '/',
+                '/page2',
+                '/',
+                '/page2',
+                '/',
+                '/page3',
+                '/'
+            ]);
+            assert.deepEqual(pdInstance.determine(), '/page2');
+        });
+
+    });
+
+    describe('alias functions', function() {
+        describe('here()', function() {
+            it('is an alias of observe()', function() {
+                assert.equal(pdInstance.here, pdInstance.observe);
+            });
+        });
+        describe('next()', function() {
+            it('is an alias of determine()', function() {
+                assert.equal(pdInstance.next, pdInstance.determine);
+            });
+        });
+    });
+
+    function doJourney(steps) {
+        steps.forEach(function(url) {
+            pdInstance.observe(url);
+        });
+    }
+
+});
